c567f8cf0f638ef36d1cba77df2dd40c1790de06
Akash donge <114501810+Akashdonge@users.noreply.github.com>
2024-04-02 08:02:57 +0530

    Update program13.c

--- program13.c
+++ program13.c
@@ -33,15 +33,30 @@ struct node* pop(struct node* head){
      free(head);
      return temp;
 }
+void display(struct node* head){
+    while(head!=NULL){
+        printf("%d ",head->data);
+        head=head->next;
+    }
+    printf("\n");
+}
 void main(){
-    struct node* top=NULL;
-   top= push(top,34);
-   top= push(top,35);
-   top= push(top,36);
-   top= push(top,37);
-    top=pop(top);
-    top=pop(top);
-    top=pop(top);
-    top=pop(top);
-    top=pop(top);
+    struct node* head=NULL;
+  int data,choice;
+   while(choice!=4){
+    printf("enter 1 for push \n 2 for pop \n 3 for dispaly\n 4 for exit \n");
+    scanf("%d",&choice);
+    switch(choice){
+        case 1: printf("enter data to push\n");
+                scanf("%d",&data);
+                head=push(head,data);
+                break;
+        case 2: head=pop(head);
+                break;
+        case 3: display(head);
+                break;
+        case 4: printf("Exiting the program");
+         break;                
+    }
+   }
 }
